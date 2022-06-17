8b88ca13f08371e61d3fc3977b63c504ce763c85
Eduardo miguel De almeida polvora pereira carneiro <ede-alme@c1r5s2.42lisboa.com>
2022-06-17 23:27:37 +0100

    17/06

--- clients/client.c
+++ clients/client.c
@@ -6,13 +6,48 @@
 /*   By: ede-alme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/21 18:08:38 by ede-alme          #+#    #+#             */
-/*   Updated: 2022/05/21 18:47:27 by ede-alme         ###   ########.fr       */
+/*   Updated: 2022/06/17 21:41:26 by ede-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdio.h>
+#include "../includes/minitalk.h"
 
-int	main(void)
+int	ft_atoi(const char *str)
 {
-	printf("Teste de cliente\n");
+	int	number;
+	int	signal;
+
+	signal = 0;
+	number = 0;
+	while (*str == ' ' || *str == '\n' || *str == '\t' || *str == '\v'
+		|| *str == '\f' || *str == '\r')
+		str++;
+	if (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			signal = -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+	{
+		number = (number * 10) + ((int) *str - 48);
+		str++;
+	}
+	if (signal == -1)
+		return (signal * number);
+	return (number);
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc != 3)
+		printf("You should put PID as argument and text\n");
+	else
+	{
+		pid = atoi(argv[argc - 2]);
+		printf("Arg PID -> %i\n", pid);
+	}
+	return (0);
 }
--- includes/minitalk.h
+++ includes/minitalk.h
@@ -1,28 +1,21 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   server.c                                           :+:      :+:    :+:   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ede-alme <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/05/21 18:09:32 by ede-alme          #+#    #+#             */
-/*   Updated: 2022/06/17 21:57:35 by ede-alme         ###   ########.fr       */
+/*   Created: 2022/05/24 20:03:46 by ede-alme          #+#    #+#             */
+/*   Updated: 2022/05/24 20:46:45 by ede-alme         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/minitalk.h"
-#include <unistd.h>
-#include <stdlib.h>
-#include <signal.h>
+#ifndef MINITALK_H
+# define MINITALK_H
+# include <stdio.h>
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <signal.h>
 
-int	main(void)
-{
-	int	pid;
-
-	pid = getpid();
-	printf("ID from server: -> %d\n", pid);
-	while (1)
-	{
-		pause();
-	}
-}
+#endif
